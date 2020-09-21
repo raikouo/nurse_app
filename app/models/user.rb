8af94199df2b09b2fdb493ba81372d22a0446f67
raikouo <gogotea4949@gmail.com>
2020-09-21 21:22:11 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,6 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
 
   validates :nickname, uniqueness: { case_sensitive: true }, presence: true
-  validates :password, format: { with: PASSWORD_REGEX, message: '半角英数字混在、6文字以上で入力してください' }
+  validates :password, format: { with: PASSWORD_REGEX, message: '半角英数字混在、6文字以上で入力してください' },
+                        if: -> { changes['encrypted_password'] }, allow_blank: true
 end
